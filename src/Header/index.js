0c0ed6282e916f5f5a0ae9ad9d11c5bcd206a17d
Keven Jesus <falecom@keven.com.br>
2018-05-12 15:43:13 -0300

    Update index.js

--- src/Header/index.js
+++ src/Header/index.js
@@ -4,23 +4,17 @@ import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 
 const ButtonAppBar = props => (
-    <div>
       <AppBar position="static">
         <Toolbar>
-          <IconButton color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="title" color="inherit">
             Title
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
-    </div>
 )
 
 
