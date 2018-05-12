14f79e61fed87caa7748dc005431c3eab2c3a9fc
Keven Jesus <falecom@keven.com.br>
2018-05-12 15:39:37 -0300

    Add files via upload

---
+++ src/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const ButtonAppBar = props => (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="title" color="inherit">
+            Title
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+)
+
+
+export default ButtonAppBar;
